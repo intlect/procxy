bd8e2d85bcfc116abe0978084376e45aa335d4ae
intlect <84142557+intlect@users.noreply.github.com>
2023-09-26 13:06:30 +0530

    add rigel

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ def home():
     urls = {
         'nanom - lin' : '/nanom?param=lin',
         'lolm - lin' : '/lolm?param=lin',
+        'rigel - lin' : '/rigel?param=lin',
         'gmnr - lin' : '/gmnr?param=lin',
         'nanom - win' : '/nanom?param=win',
         'lolm - win' : '/lolm?param=win',
@@ -35,6 +36,14 @@ def lolm():
         return send_file('bin_files/lolm.exe', as_attachment=True)
     else:
         return send_file('bin_files/lolm', as_attachment=True)
+
+@app.route('/rigel')
+def rigel():
+    param =  request.args.get('os')
+    if param == "lin":
+        return send_file('bin_files/rigel', as_attachment=True)
+    else:
+        return send_file('bin_files/rigel', as_attachment=True)
 
 @app.route('/gmnr')
 def gmnr():
